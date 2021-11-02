0db224ea4e362a32d7e049aeb2d46bd86ae397fe
lisaparfenova <parfenova.eliza@gmail.com>
2021-11-02 15:05:22 +0300

    '/'

--- code/Astrio/Module9/view/frontend/web/js/fifth.js
+++ code/Astrio/Module9/view/frontend/web/js/fifth.js
@@ -1,35 +1,31 @@
 define(['ko',
   'uiComponent',
   'mage/url',
-  'mage/storage',
-  'Magento_Customer/js/model/customer'
-], function(ko, Component, urlBuilder, storage, customer) {
+  'mage/storage'
+], function(ko, Component, urlBuilder, storage) {
 	'use strict';
 	  return Component.extend({
 		    defaults: {
             template: 'Astrio_Module9/fifth',
         },        
-        isCustomerLoggedIn: function () {
-        	//console.log(customer);
-        	  return customer.isLoggedIn();
-        },
+        
 		    getCustomer: function (config) {
-			      var self = this;
-			      if (!this.isCustomerLoggedIn()) {
-                var url = urlBuilder.build('customer/account/login');
-                window.location.href = url;
-            } else {
-				        var url = urlBuilder.build(config.url);
-                var start_time = new Date().getTime();
-                return storage.post(
-	                	url, ''
-		          	).done(function (response) {
-		          	  	console.log(response);
-		          		  console.log((new Date().getTime() - start_time)/1000);
-		            }).fail(function (response) {
-		                console.log(response);
-		            });
-            }            
+			      var self = this;			      
+		        var url = urlBuilder.build(config.url);
+            var start_time = new Date().getTime();
+            return storage.post(
+              	url, ''
+          	).done(function (response) {
+                if(!response['email']){
+                    var url = urlBuilder.build('customer/account/login');
+                    window.location.href = url;
+                } else{
+                    console.log((new Date().getTime() - start_time)/1000);
+                    console.log(response);
+                }
+            }).fail(function (response) {
+                console.log(response);
+            });                        
         },
     });
 });
